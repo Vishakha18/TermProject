6301c06e5d626db52b2a4f3012d60e5efb929579
Vishakha Mehan <Vishakha Mehan@VishakhaMehan.gateway.2wire.net>
2015-08-02 18:28:20 -0500

    Test.java with service method

---
+++ src/java/com/test/Test.java
@@ -0,0 +1,36 @@
+package com.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class Test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+   
+    public Test() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	
+		doPost(request, response);
+	}
+
+	
+}
