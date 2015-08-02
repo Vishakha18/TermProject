5ba4c1d065e1764cf96cafb3d72973955c2170bb
Vishakha Mehan <Vishakha Mehan@VishakhaMehan.gateway.2wire.net>
2015-08-02 17:07:33 -0500

    Modified hr.java

--- src/java/com/test/Hr.java
+++ src/java/com/test/Hr.java
@@ -38,4 +38,14 @@ public class Hr extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
+        /**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection con=null;
+		PreparedStatement ps=null;
+		PrintWriter pw=null;
+		       String Username=request.getParameter("username");
+		       String Password=request.getParameter("password");
+        }
 }
