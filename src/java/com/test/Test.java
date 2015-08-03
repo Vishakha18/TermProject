2533eb16fb3922c8037cd436b37ea3cf13f23a18
Vishakha Mehan <Vishakha Mehan@VishakhaMehan.gateway.2wire.net>
2015-08-02 19:13:53 -0500

    Final Test.java

--- src/java/com/test/Test.java
+++ src/java/com/test/Test.java
@@ -39,4 +39,47 @@ public class Test extends HttpServlet {
 		// TODO Auto-generated method stub
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		System.out.println("dffb");
+		String Fname=request.getParameter("name");
+		String Lname=request.getParameter("name1");
+		String Email=request.getParameter("email");
+		String Qualification=request.getParameter("edu");
+		String Percentage=request.getParameter("per");
+		String MobileNo =request.getParameter("mobile");
+		
+	try {
+		PrintWriter pw=response.getWriter();
+		ResultSet rs=null;
+		Class.forName("com.mysql.jdbc.Driver");
+                String url="jdbc:mysql://ipro.lambton.on.ca/inventory";
+                String username="products";
+                String password="products";
+		Connection con=DriverManager.getConnection(url,username,password);
+		pw.write("Connection Established"+con);
+		
+		PreparedStatement ps=con.prepareStatement("insert into Student values(Student1.nextval,?,?,?,?,?,?)");
+		ps.setString(1,Fname);
+		ps.setString(2,Lname);
+		ps.setString(3,Email);
+		ps.setString(4,Qualification);
+		ps.setString(5,Percentage);
+		ps.setString(6,MobileNo);
+		rs=ps.executeQuery();
+			response.sendRedirect("Test.jsp");
+		
+	} catch (ClassNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+		
+	}
+
 }
