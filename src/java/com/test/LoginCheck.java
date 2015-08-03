cb3c33db0237e2b0d6aa480d9572010aab41cbd7
Vishakha Mehan <Vishakha Mehan@VishakhaMehan.gateway.2wire.net>
2015-08-03 14:17:56 -0500

    LoginCheck.java

---
+++ src/java/com/test/LoginCheck.java
@@ -0,0 +1,29 @@
+package com.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginCheck extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginCheck() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+}
