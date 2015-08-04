bf00840c2dcd148fc9fc389499bac5bcd10a9acf
Vishakha Mehan <Vishakha Mehan@VishakhaMehan.gateway.2wire.net>
2015-08-04 01:02:35 -0500

    Finally modified and Tested Hr.java file with some error messages to be displayed

--- src/java/com/test/Hr.java
+++ src/java/com/test/Hr.java
@@ -42,85 +42,13 @@ public class Hr extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Connection con=null;
-		PreparedStatement ps=null;
-		PrintWriter pw=null;
-		       String Username=request.getParameter("username");
-		       String Password=request.getParameter("password");
-        
-        if(Username.equals("hradmin")){
-		try {
-			pw=response.getWriter();
-			Class.forName("com.mysql.jdbc.Driver");
-                        String url="jdbc:mysql://localhost:3306/hr";
-                        String username="root";
-                        String password="";
-			con=DriverManager.getConnection(url,username,password);	
-			 ps=con.prepareStatement("Select * from Student ");
-			ResultSet rs=ps.executeQuery();
-			pw.println("<html>");
-			
-			String d="<table width='500' border='1' cellspacing='0' cellpadding='0'>";
-			
-			pw.println(d); 
-			pw.println("<tr>");
-			pw.println("<th>"+"S.No."+"</th>"); 
-			pw.println("<th>"+"First Name"+"</th>"); 
-			pw.println("<th>"+"Last Name"+"</th>"); 
-			pw.println("<th>"+"Email Id"+"</th>"); 
-			pw.println("<th>"+"Qualification"+"</th>"); 
-			pw.println("<th>"+"Percentage"+"</th>"); 
-			pw.println("<th>"+"Mobile Number"+"</th>"); 
-			pw.println("<tr>");
-			while(rs.next()) 
-			{ 
-			pw.println("<td>"+rs.getString(1)+"</td>"); 
-			pw.println("<td>"+rs.getString(2)+"</td>"); 
-			pw.println("<td>"+rs.getString(3)+"</td>"); 
-			pw.println("<td>"+rs.getString(4)+"</td>"); 
-			pw.println("<td>"+rs.getString(5)+"</td>"); 
-			pw.println("<td>"+rs.getString(6)+"</td>"); 
-			pw.println("<td>"+rs.getString(7)+"</td>"); 
-			
-			pw.println("</tr>");
-			
-			//pw.println("<tr>");
-			} 
-			pw.println("</table></html>"); 
-			
-			
-		}catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-                catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		       }else{
-		    	   response.sendRedirect("hr.jsp");
-		       }
-		       
-		       try {
-				PreparedStatement ps1=con.prepareStatement("select Percentage from Student where Percentage >60");
-			
-					ResultSet rs1=ps1.executeQuery();
-					while(rs1.next()) 
-					{ 
-	                  //  for()    
-					pw.println(rs1.getString(1));
-					
-
-								
-						
-					}
-		       }
-				 catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}   
-		       
-		
+            if(request.getParameter("username").equals("Admin") && request.getParameter("password").equals("Admin")){
+                request.getRequestDispatcher("poster.jsp").forward(request, response);
+            }
+            else{
+                request.setAttribute("invalid", "Password or Username  is Incorrect");
+                request.getRequestDispatcher("hr.jsp").forward(request, response);
+            }
 	}
 
         }
